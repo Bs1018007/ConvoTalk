d3d15b3c272057eec6545570df3b901247a48f7a
Bs1018007 <bs1018007@gmail.com>
2025-07-16 22:41:44 +0530

    8

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:3000"
-  : "http://54-234-229-97.nip.io:3000";
+  : "http://convotalk-1.onrender.com:3000";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
